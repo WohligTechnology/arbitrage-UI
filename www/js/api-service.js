4ac48a2efe196bf626869cc1869b145dad7d9c83
Chintan Shah <chintan@wohlig.com>
2018-11-06 19:04:50 +0100

    ok

--- www/js/api-service.js
+++ www/js/api-service.js
@@ -1,14 +1,15 @@
-angular.module('apiService', [])
-  .factory('apiService', function ($http, $timeout, $state) {
+angular
+  .module("apiService", [])
+  .factory("apiService", function($http, $timeout, $state) {
     var scripts = {
-      "BCN": {
-        currencyShortName1: "BCN",
+      BTG: {
+        currencyShortName1: "BTG",
         currencyShortName2: "BTC",
-        url: "https://bcn.wohlig.in",
-        currency1: "Bytecoin",
+        url: "https://btg.wohlig.in",
+        currency1: "BitcoinGold",
         currency2: "Bitcoin",
-        initialCurrency1: 306900.000000000,
-        initialCurrency2: 0.17232397,
+        initialCurrency1: 0,
+        initialCurrency2: 2,
         initialDate: "07-24-2018",
         currency1Decimal: 0,
         currency2Decimal: 8,
@@ -17,13 +18,13 @@ angular.module('apiService', [])
       }
     };
     return {
-      setScript: function (script, callback) {
+      setScript: function(script, callback) {
         $.jStorage.set("script", scripts[script]);
-        $timeout(function () {
+        $timeout(function() {
           callback();
         }, 100);
       },
-      getScript: function () {
+      getScript: function() {
         var scriptDet = $.jStorage.get("script");
         if (scriptDet) {
           adminurl = scriptDet.url + "/api/";
@@ -32,75 +33,81 @@ angular.module('apiService', [])
           $state.go("app.home");
         }
       },
-      balance: function (callback) {
+      balance: function(callback) {
         $http({
-          url: adminurl + 'Balance/getOldNew',
-          method: 'POST'
-        }).then(function (data) {
+          url: adminurl + "Balance/getOldNew",
+          method: "POST"
+        }).then(function(data) {
           callback(null, data.data.data);
         });
       },
-      getLastMarketData: function (callback) {
+      getLastMarketData: function(callback) {
         $http({
-          url: adminurl + 'MarketsData/getLast',
-          method: 'POST'
-        }).then(function (data) {
+          url: adminurl + "MarketsData/getLast",
+          method: "POST"
+        }).then(function(data) {
           callback(null, data.data.data);
         });
       },
-      getKoinex: function (callback) {
+      getKoinex: function(callback) {
         $http({
           url: "https://koinex.in/api/ticker",
-          method: 'GET'
-        }).then(function (data) {
+          method: "GET"
+        }).then(function(data) {
           callback(null, data.data);
         });
       },
-      getArbitrage: function (callback) {
+      getArbitrage: function(callback) {
         $http({
-          url: adminurl + 'Arbitrage/getArbitrage',
-          method: 'GET'
-        }).then(function (data) {
+          url: adminurl + "Arbitrage/getArbitrage",
+          method: "GET"
+        }).then(function(data) {
           callback(null, data.data);
         });
       },
-      getBestScripts: function (callback) {
-        $http.post(adminurl + "MarketsData/getBestScripts").then(function (data) {
-          callback(data.data.data);
-        });
+      getBestScripts: function(callback) {
+        $http
+          .post(adminurl + "MarketsData/getBestScripts")
+          .then(function(data) {
+            callback(data.data.data);
+          });
       },
-      searchProcess: function (page, callback) {
-        $http.post(adminurl + "Process/search", {
-          page: page,
-          // filter: {
-          //   status: "ProcessCompleted"
-          // }
-
-        }).then(function (data) {
-          callback(data.data.data);
-        });
+      searchProcess: function(page, callback) {
+        $http
+          .post(adminurl + "Process/search", {
+            page: page
+            // filter: {
+            //   status: "ProcessCompleted"
+            // }
+          })
+          .then(function(data) {
+            callback(data.data.data);
+          });
       },
-      cancelTransaction1: function (id, callback) {
-        $http.post(adminurl + "Transaction/checkTransaction1", {
-          _id: id
-        }).then(function (data) {
-          callback(data.data.data);
-        });
+      cancelTransaction1: function(id, callback) {
+        $http
+          .post(adminurl + "Transaction/checkTransaction1", {
+            _id: id
+          })
+          .then(function(data) {
+            callback(data.data.data);
+          });
       },
-      cancelTransaction2: function (id, callback) {
-        $http.post(adminurl + "Transaction/checkTransaction2", {
-          _id: id
-        }).then(function (data) {
-          callback(data.data.data);
-        });
+      cancelTransaction2: function(id, callback) {
+        $http
+          .post(adminurl + "Transaction/checkTransaction2", {
+            _id: id
+          })
+          .then(function(data) {
+            callback(data.data.data);
+          });
       },
-      getDataPerDay: function (callback) {
+      getDataPerDay: function(callback) {
         // var adminurl = "http://localhost:1337/api/";
-        $http.post(adminurl + "Balance/getDataPerDay").then(function (data) {
+        $http.post(adminurl + "Balance/getDataPerDay").then(function(data) {
           callback(data.data.data);
         });
-
-      },
+      }
       // This is a demo Service for POST Method.
     };
   });
