ef8b99faea2873a73de377374fc573b7fbff8295
Chintan Shah <chintan@wohlig.com>
2018-11-06 19:09:29 +0100

    ok

--- www/js/api-service.js
+++ www/js/api-service.js
@@ -10,7 +10,7 @@ angular
         currency2: "Bitcoin",
         initialCurrency1: 0,
         initialCurrency2: 2,
-        initialDate: "07-24-2018",
+        initialDate: "11-07-2018",
         currency1Decimal: 0,
         currency2Decimal: 8,
         rateDecimal: 10,
